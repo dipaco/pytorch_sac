32f496019fb077583b8f989391b6cbd99157c44a
Diego Patino <dipaco@gmail.com>
2022-05-18 15:55:52 -0400

    Async rendering

--- video.py
+++ video.py
@@ -2,8 +2,16 @@ import imageio
 import os
 import numpy as np
 import sys
+from multiprocessing import Process, Manager
 
 import utils
+
+
+def _render_async(render_method, arg_list, path, fps):
+    frames = []
+    for i, args in enumerate(arg_list):
+        frames.append(render_method(args))
+    imageio.mimsave(path, frames, fps=fps)
 
 class VideoRecorder(object):
     def __init__(self, root_dir, height=256, width=256, camera_id=0, fps=30):
@@ -14,9 +22,13 @@ class VideoRecorder(object):
         self.fps = fps
         self.frames = []
 
-    def init(self, enabled=True):
+    def init(self, enabled=True, async_recorder=True):
         self.frames = []
         self.enabled = self.save_dir is not None and enabled
+        self.async_recorder = async_recorder
+        self.frame_args = []
+        self.render_method = None
+        self.process_manager = Manager()
 
     def record(self, env):
         if self.enabled:
@@ -24,10 +36,25 @@ class VideoRecorder(object):
                                height=self.height,
                                width=self.width,
                                camera_id=self.camera_id)'''
-            frame = env.render(mode='rgb_array')
-            self.frames.append(frame)
+
+            if self.async_recorder:
+                self.frame_args.append(env.get_render_args())
+                if self.render_method is None:
+                    self.render_method = type(env).render_async
+            else:
+                frame = env.render(mode='rgb_array')
+                self.frames.append(frame)
 
     def save(self, file_name):
         if self.enabled:
-            path = os.path.join(self.save_dir, file_name)
-            imageio.mimsave(path, self.frames, fps=self.fps)
+            if self.async_recorder:
+                pr = Process(
+                    target=_render_async,
+                    args=(self.render_method, self.frame_args, os.path.join(self.save_dir, file_name), self.fps)
+                )
+                pr.start()
+            else:
+                path = os.path.join(self.save_dir, file_name)
+                imageio.mimsave(path, self.frames, fps=self.fps)
+
+
